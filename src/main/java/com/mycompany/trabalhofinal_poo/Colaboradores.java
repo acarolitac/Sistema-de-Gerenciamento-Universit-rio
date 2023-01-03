fde1936ab7599eaa40a98c5a0748b9b562e7aeaa
Ana Carolina <anacarolinaitacarambi@gmail.com>
2023-01-03 09:13:50 -0300

    Colaboradores e vetores
    Criação da classe Colaboradores e criação de vetores na classe teste (Sistema).

--- src/main/java/com/mycompany/trabalhofinal_poo/Colaboradores.java
+++ src/main/java/com/mycompany/trabalhofinal_poo/Colaboradores.java
@@ -8,6 +8,39 @@ package com.mycompany.trabalhofinal_poo;
  *
  * @author Ana Carolina, Marcela e Júlia
  */
-public class Colaboradores {
-    
+public class Colaboradores extends Pessoa {
+
+    private String funcionarios;
+    private String cargo;
+
+    public Colaboradores(String nomeCompleto, String cpf, String endereco, String telefone) {
+        super(nomeCompleto, cpf, endereco, telefone);
+        this.funcionarios = funcionarios;
+        this.cargo = cargo;
+    }
+
+    public String getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(String funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    @Override
+    public void mostrarDados() {
+        System.out.println("\nColaboradores:");
+        super.mostrarDados();
+        System.out.println("Funcionário: " + funcionarios);
+        System.out.println("Cargo na instituição: " + cargo);
+    }
+
 }
--- src/main/java/com/mycompany/trabalhofinal_poo/Sistema.java
+++ src/main/java/com/mycompany/trabalhofinal_poo/Sistema.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.trabalhofinal_poo;
-
+import java.util.Arrays;
 /**
  *
  * @author Ana Carolina, Marcela e Júlia
@@ -11,9 +11,45 @@ package com.mycompany.trabalhofinal_poo;
 public class Sistema {
 
     public static void main(String[] args) {
-        Aluno a1 = new Aluno("Ana", "000-000-000.00", "Brasília", "123");
-        a1.setCurso("Sistemas para Internet");
-        a1.setMatricula(123456789);
-        a1.mostrarDados();
+
+        String[] strings = new String[3];
+
+        Aluno[] al = new Aluno[3];
+        Aluno al1 = new Aluno("Ana", "0434343", "Brasília", "123433");
+        Aluno al2 = new Aluno("Júlia", "0434343", "Brasília", "123433");
+        Aluno al3 = new Aluno("Marcela", "0434343", "Brasília", "123433");
+
+        al1.setNome("Ana");
+        al2.setNome("Júlia");
+        al3.setNome("Marcela");
+        al1.setCurso("Sistemas para internet");
+        al2.setCurso("Administração");
+        al3.setCurso("ETC");
+
+        al1.setMatricula(01234);
+        al2.setMatricula(45670);
+        al3.setMatricula(89700);
+
+        System.out.println("Aluno(a) 1:" + "\nNome: " + al1.getNome() + "\nCurso: " + al1.getCurso() + "\nMatricula: " + al1.getMatricula());
+        System.out.println("\nAluno(a) 2:" + "\nNome: " + al2.getNome() + "\nCurso: " + al2.getCurso() + "\nMatricula: " + al2.getMatricula());
+        System.out.println("\nAluno(a) 3:" + "\nNome: " + al3.getNome() + "\nCurso: " + al3.getCurso() + "\nMatricula: " + al3.getMatricula());
+
+        Professor[] prof = new Professor[3];
+        Professor prof1 = new Professor("João", "0434343", "Brasília", "123433");
+        Professor prof2 = new Professor("Maria", "23434343", "Taguatinga", "123456667");
+        Professor prof3 = new Professor("José", "2113434342", "Asa Sul", "1234432");
+        prof1.setNome("João");
+        prof1.setNome("Maria");
+        prof1.setNome("José");
+        prof1.setDisciplina("POO");
+        prof2.setDisciplina("SAA");
+        prof3.setDisciplina("BDII");
+        prof1.setCargaHoraria(70);
+        prof2.setCargaHoraria(70);
+        prof3.setCargaHoraria(70);
+        System.out.println("\nDocente 1:" + "\nNome: " + prof1.getNome() + "\nDisciplina: " + prof1.getDisciplina() + "\nCarga Horaria: " + prof1.getCargaHoraria());
+        System.out.println("Docente 2:" + "\nNome: " + prof2.getNome() + "\nDisciplina: " + prof2.getDisciplina() + "\nCarga Horaria: " + prof2.getCargaHoraria());
+        System.out.println("Docente 3:" + "\nNome: " + prof3.getNome() + "\nDisciplina: " + prof3.getDisciplina() + "\nCarga Horaria: " + prof3.getCargaHoraria());
+
     }
 }
